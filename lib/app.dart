d0e634e90fa8b11d2298129f532f265888f06880
MD ZAHED IQBAL <tajiniiuc222@gmail.com>
2026-03-31 09:34:46 +0600

    theme data added

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager_apps/presentation/screens/main_nav_bar_screen.dart';
+import 'package:task_manager_apps/presentation/screens/sign_up_screen.dart';
+import 'package:task_manager_apps/presentation/screens/splash_screen.dart';
 
 class TaskManagerApp extends StatelessWidget {
   const TaskManagerApp({super.key});
+
+  static GlobalKey<NavigatorState> navigator = GlobalKey<NavigatorState>();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      navigatorKey: navigator,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorSchemeSeed: Colors.green,
+        textTheme: TextTheme(
+          titleLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: Colors.white,
+          filled: true,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+          border: OutlineInputBorder(borderSide: BorderSide.none),
+          enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+          errorBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+            backgroundColor: Colors.green,
+            fixedSize: Size.fromWidth(double.maxFinite),
+            padding: EdgeInsets.symmetric(vertical: 12),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
       ),
-      home: const MainNavBarScreen(),
-      initialRoute: MainNavBarScreen.name,
+      initialRoute: SplashScreen.name,
+      routes: {
+        MainNavBarScreen.name: (_) => MainNavBarScreen(),
+        SignUpScreen.name: (_) => SignUpScreen(),
+      },
     );
   }
 }
